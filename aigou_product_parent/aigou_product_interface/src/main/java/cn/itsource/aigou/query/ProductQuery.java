96744656c8c1aff01dc33b9636027227a658982a
xwg <lwg@126.com>
2019-02-27 19:06:24 +0800

    第四天

---
+++ aigou_product_parent/aigou_product_interface/src/main/java/cn/itsource/aigou/query/ProductQuery.java
@@ -0,0 +1,10 @@
+package cn.itsource.aigou.query;
+
+import cn.itsource.aigou.query.BaseQuery;
+/**
+ *
+ * @author wbtest
+ * @since 2019-02-27
+ */
+public class ProductQuery extends BaseQuery{
+}
